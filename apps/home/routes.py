8ce6192dadf7ec47d3d002f2e8c186738f4a25ea
MahiSharma000 <sharma02Mahi@gmail.com>
2023-02-08 16:55:53 +0530

    edited dashboard

--- apps/home/routes.py
+++ apps/home/routes.py
@@ -4,7 +4,7 @@ Copyright (c) 2019 - present AppSeed.us
 """
 
 from apps.home import blueprint
-from flask import render_template, request
+from flask import render_template, request, redirect
 from flask_login import login_required
 from jinja2 import TemplateNotFound
 from apps.authentication.models import *
@@ -13,8 +13,7 @@ from apps.authentication.models import *
 @blueprint.route('/index')
 @login_required
 def index():
-
-    return render_template('home/index.html', segment='index')
+    return redirect('/admin/dashboard')
 
 
 @blueprint.route('/<template>')
