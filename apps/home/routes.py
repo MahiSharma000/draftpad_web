5e8d23d2c8e92df1ae487da0e233aa3ea0119748
MahiSharma000 <sharma02Mahi@gmail.com>
2023-04-03 22:35:34 +0530

    updated

--- apps/home/routes.py
+++ apps/home/routes.py
@@ -6,6 +6,7 @@ Copyright (c) 2019 - present AppSeed.us
 import base64
 import io
 from tkinter import Image
+from apps.authentication.forms import AddMemberForm, CreateAccountForm
 from apps.home import blueprint
 from flask import render_template, request, redirect, url_for
 from flask_login import login_required
@@ -98,12 +99,38 @@ def chaptercontent(id):
     chapter = Chapter.query.get(id)
     return render_template('home/chapterContent.html', chapter=chapter)
 
+#get comments by chapter id
+@blueprint.route('/admin/chapterComments/<int:id>')
+def chapterComments(id):
+    comments = Comment.query.filter_by(chapter_id=id).all()
+    for comment in comments:
+        chapter = Chapter.query.get(comment.chapter_id)
+        user = Users.query.get(comment.user_id)
+        comment.chapter = chapter
+        comment.user = user
+    return render_template('home/chapterComments.html', comments=comments)
+
 @blueprint.route('/user/<int:id>/action/delete')
 def userDelete(id):
     user = Users.query.get(id)
     db.session.delete(user)
     db.session.commit()
     return redirect('/admin/users')
+
+@blueprint.route('/follow/<int:id>/action/delete')
+def followDelete(id):
+    follow = Follower.query.get(id)
+    db.session.delete(follow)
+    db.session.commit()
+    return redirect('/admin/users')
+
+#delete chapter
+@blueprint.route('/chapter/<int:id>/action/delete')
+def chapterDelete(id):
+    chapter = Chapter.query.get(id)
+    db.session.delete(chapter)
+    db.session.commit()
+    return redirect('/admin/books')
 
 #edit user
 @blueprint.route('/user/<int:id>/action/edit', methods=['GET', 'POST'])
@@ -196,14 +223,31 @@ def reportDetails(id):
 
 @blueprint.route('/admin/member/add', methods=['GET', 'POST'])
 def addMember():
-    if request.method == 'POST':
-        email = request.form['email']
+    create_account_form = CreateAccountForm(request.form)
+    if 'register' in request.form:
+
         username = request.form['username']
-        password = request.form['password']
-        user = Users(email=email, username=username, password=password)
+        email = request.form['email']
+
+        # Check usename exists
+        user = Users.query.filter_by(username=username).first()
+        if user:
+            return render_template(msg='Username already registered')
+
+        # Check email exists
+        user = Users.query.filter_by(email=email).first()
+        if user:
+            return render_template(msg='Email already registered',)
+
+        # else we can create the user
+        user = Users(**request.form)
         db.session.add(user)
         db.session.commit()
-        return redirect('/admin/users')
+
+        return render_template('accounts/register.html',
+                               msg='User created please <a href="/login">login</a>',
+                               success=True,
+                               form=create_account_form)
 
     return render_template('home/add_new_member.html')
 
